46668989fa0f23311cc676ebaaf1c550f15408e0
Charlotte Vanroelen <charlili@Charlovin-it.local>
2015-12-04 10:27:50 +0100

    Colortracking: works. Change color in script.js. Closed #6

--- tests/colortracking/_js/script.js
+++ tests/colortracking/_js/script.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
 
-import {mobileCheck, removeByClassName} from './helpers/util';
+import {mobileCheck} from './helpers/util';
 
 //import Video from './modules/Video';
 import Status from '../models/Status';
 let helper;
 let socket, passcode;
+//let tracking = require('tracking');
 
 const initSocket = () => {
 
@@ -42,6 +41,29 @@ const setMobile = () => {
 
 const setComputer = () => {
   console.log(socket);
+
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var tracker = new tracking.ColorTracker();
+  tracking.track('#video', tracker, { camera: true });
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    event.data.forEach(function(rect) {
+      /*if (rect.color === 'custom') {
+        rect.color = tracker.customColor;
+      }*/
+      rect.color = '#D86198';
+      context.strokeStyle = rect.color;
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
+  //initGUIControllers(tracker);
+
 
   setStatus('searching');
 
